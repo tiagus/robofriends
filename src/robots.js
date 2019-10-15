438a416a938528c53b0b19dee223744b8508ac1c
Tiago Santos <tiago@convertodigital.com>
2019-10-15 13:37:01 +0100

    update

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Tiago Santos',
+    username: 'Tiago',
+    email: 'tsantos@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'José Vieira',
+    username: 'Jose',
+    email: 'jvieira@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Luis Marques',
+    username: 'Luis',
+    email: 'lmarques@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Bruno Santos',
+    username: 'Bruno',
+    email: 'bsantos@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
