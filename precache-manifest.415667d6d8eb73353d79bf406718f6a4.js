4532ed2fe0d15c5d41ad7846df38f0ebb83f3301
Tiago Santos <tiago@convertodigital.com>
2019-10-16 12:41:23 +0100

    Updates

---
+++ precache-manifest.415667d6d8eb73353d79bf406718f6a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e8ad8ce5794fe618e1912329af3219",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "70c35a481f1d7edeccef",
+    "url": "/robofriends/static/css/main.0de05387.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "70c35a481f1d7edeccef",
+    "url": "/robofriends/static/js/main.f30820f7.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
