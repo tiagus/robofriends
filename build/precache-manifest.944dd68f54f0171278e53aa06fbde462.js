c5ff8dec320d2eb2b294e88c11ee6d6a092aaaac
Tiago Santos <tiago@convertodigital.com>
2019-10-16 11:30:22 +0100

    upload build folder

---
+++ build/precache-manifest.944dd68f54f0171278e53aa06fbde462.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5528baec0a9b1b60a30bba8621e59132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "05ea62db63fa74cee330",
+    "url": "/static/css/main.9f3170d6.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "05ea62db63fa74cee330",
+    "url": "/static/js/main.67bc7f5b.chunk.js"
+  },
+  {
+    "revision": "05efcc9d811b4a2a3c6e",
+    "url": "/static/js/runtime-main.8f8b62bf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
